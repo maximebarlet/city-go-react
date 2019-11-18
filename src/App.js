6058808f89cec9043b697fcb8f8742c02d656c86
maximebarlet <44022578+maximebarlet@users.noreply.github.com>
2019-11-18 09:56:51 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ export default function App() {
       <Navigation />
       <div className="map-container">
           <MapWrapped
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCH7anqaqx11AqSPHR1rzBACnVHzhbsJl4`}
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=`}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100%` }} />}
             mapElement={<div style={{ height: `100%` }} />}
